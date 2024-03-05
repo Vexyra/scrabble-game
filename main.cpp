e8794d400149980c59b329cec5b8708cc47e1ca8
Vexyra <malakoiteobald@gmail.com>
2024-03-04 19:25:52 -0600

    Cleaned up code, removed cout for debugging purposes

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ struct Data {
 };
 
 int CalculateScore(std::string sPlayerInput) {
-    Data ScrabbleGame[26] = {
+    Data ScrabbleGame[] = {
             {'A', 1},
             {'B', 3},
             {'C', 3},
@@ -34,19 +34,16 @@ int CalculateScore(std::string sPlayerInput) {
             {'W', 4},
             {'X', 8},
             {'Y', 4},
-            {'Z', 10},
+            {'Z', 10}
     };
 
-    int nPlayerScore;
+    int nPlayerScore = 0;
 
     for(int i = 0; i < sPlayerInput.length(); i++) {
         for (int j = 0; j < 26; j++) {
             if (sPlayerInput[i] == ScrabbleGame[j].chLetter) {
-                std::cout << "Character: " << sPlayerInput[i] << ", " << "Score: " << ScrabbleGame[j].nLetterScore
-                          << std::endl;
-                nPlayerScore+= ScrabbleGame[j].nLetterScore;
-            } else {
-                nPlayerScore+= 0;
+                nPlayerScore += ScrabbleGame[j].nLetterScore;
+                break;
             }
         }
     }
@@ -55,9 +52,6 @@ int CalculateScore(std::string sPlayerInput) {
 }
 
 int main() {
-    int nPlayer1Score;
-    int nPlayer2Score;
-
     std::string sPlayer1Input;
     std::string sPlayer2Input;
 
@@ -65,13 +59,13 @@ int main() {
     std::cin >> sPlayer1Input;
 
     std::transform(sPlayer1Input.begin(), sPlayer1Input.end(), sPlayer1Input.begin(), ::toupper);
-    nPlayer1Score = CalculateScore(sPlayer1Input);
+    int nPlayer1Score = CalculateScore(sPlayer1Input);
 
     std::cout << "Player 2: ";
     std::cin >> sPlayer2Input;
 
     std::transform(sPlayer2Input.begin(), sPlayer2Input.end(), sPlayer2Input.begin(), ::toupper);
-    nPlayer2Score = CalculateScore(sPlayer2Input);
+    int nPlayer2Score = CalculateScore(sPlayer2Input);
 
     if(nPlayer1Score > nPlayer2Score) {
         std::cout << "Player 1 Wins!";
